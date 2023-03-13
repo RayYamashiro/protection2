ce738fe7c41c08088ced99c6b68d78d0459be551
RayYamashiro <vlad_alferov02@mail.ru>
2023-03-14 00:17:39 +0300

    4

--- src/main/java/org/example/RSAUtil.java
+++ src/main/java/org/example/RSAUtil.java
@@ -10,14 +10,14 @@ import java.security.spec.PKCS8EncodedKeySpec;
 import java.security.spec.X509EncodedKeySpec;
 import java.util.Base64;
 
-public  class lab2 {
+public  class RSAUtil {
         private static String publicKey = "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCgFGVfrY4jQSoZQWWygZ83roKXWD4YeT2x2p41dGkPixe73rT2IW04glagN2vgoZoHuOPqa5and6kAmK2ujmCHu6D1auJhE2tXP+yLkpSiYMQucDKmCsWMnW9XlC5K7OSL77TXXcfvTvyZcjObEz6LIBRzs6+FqpFbUO9SJEfh6wIDAQAB";
         private static String privateKey = "MIICdQIBADANBgkqhkiG9w0BAQEFAASCAl8wggJbAgEAAoGBAKAUZV+tjiNBKhlBZbKBnzeugpdYPhh5PbHanjV0aQ+LF7vetPYhbTiCVqA3a+Chmge44+prlqd3qQCYra6OYIe7oPVq4mETa1c/7IuSlKJgxC5wMqYKxYydb1eULkrs5IvvtNddx+9O/JlyM5sTPosgFHOzr4WqkVtQ71IkR+HrAgMBAAECgYAkQLo8kteP0GAyXAcmCAkA2Tql/8wASuTX9ITD4lsws/VqDKO64hMUKyBnJGX/91kkypCDNF5oCsdxZSJgV8owViYWZPnbvEcNqLtqgs7nj1UHuX9S5yYIPGN/mHL6OJJ7sosOd6rqdpg6JRRkAKUV+tmN/7Gh0+GFXM+ug6mgwQJBAO9/+CWpCAVoGxCA+YsTMb82fTOmGYMkZOAfQsvIV2v6DC8eJrSa+c0yCOTa3tirlCkhBfB08f8U2iEPS+Gu3bECQQCrG7O0gYmFL2RX1O+37ovyyHTbst4s4xbLW4jLzbSoimL235lCdIC+fllEEP96wPAiqo6dzmdH8KsGmVozsVRbAkB0ME8AZjp/9Pt8TDXD5LHzo8mlruUdnCBcIo5TMoRG2+3hRe1dHPonNCjgbdZCoyqjsWOiPfnQ2Brigvs7J4xhAkBGRiZUKC92x7QKbqXVgN9xYuq7oIanIM0nz/wq190uq0dh5Qtow7hshC/dSK3kmIEHe8z++tpoLWvQVgM538apAkBoSNfaTkDZhFavuiVl6L8cWCoDcJBItip8wKQhXwHp0O3HLg10OEd14M58ooNfpgt+8D8/8/2OOFaR0HzA+2Dm";
 
         public static PublicKey getPublicKey(String base64PublicKey){
             PublicKey publicKey = null;
             try{
-                X509EncodedKeySpec keySpec = new X509EncodedKeySpec(Base64.getDecoder().decode(base64PublicKey.getBytes()));
+                X509EncodedKeySpec keySpec = new X509EncodedKeySpec(Base64.getDecoder().decode(base64PublicKey.getBytes())); // преобразуем открытый ключ в формате base64 обратно в x509
                 KeyFactory keyFactory = KeyFactory.getInstance("RSA");
                 publicKey = keyFactory.generatePublic(keySpec);
                 return publicKey;
@@ -28,6 +28,21 @@ public  class lab2 {
             }
             return publicKey;
         }
+
+        public void RSAKeyPairGenerator() throws NoSuchAlgorithmException {
+            KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
+            keyGen.initialize(1024);
+            KeyPair pair = keyGen.generateKeyPair();
+            this.privateKey = pair.getPrivate().toString();
+            this.publicKey = pair.getPublic().toString();
+        }
+        public String getPrivateKey() {
+            return privateKey;
+        }
+        public String getPublicKey() {
+            return publicKey;
+        }
+
 
         public static PrivateKey getPrivateKey(String base64PrivateKey){
             PrivateKey privateKey = null;
@@ -64,7 +79,7 @@ public  class lab2 {
 
         public static void main(String[] args) throws IllegalBlockSizeException, InvalidKeyException, NoSuchPaddingException, BadPaddingException {
             try {
-                String encryptedString = Base64.getEncoder().encodeToString(encrypt("Dhiraj is the author", publicKey));
+                String encryptedString = Base64.getEncoder().encodeToString(encrypt("Per aspera ad astra", publicKey));
                 System.out.println(encryptedString);
                 String decryptedString = RSAUtil.decrypt(encryptedString, privateKey);
                 System.out.println(decryptedString);
@@ -73,5 +88,4 @@ public  class lab2 {
             }
 
         }
-
 }
